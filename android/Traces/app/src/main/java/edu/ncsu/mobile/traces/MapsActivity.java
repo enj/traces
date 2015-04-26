77b0121a7b79af2c1e6fefb32577dd906ffae0af
Monis Khan <monis.khan@quintiles.com>
2015-04-26 03:22:12 +0000

    Fixed getting current location on app start.  Not sure why it zooms incorrectly.

--- android/Traces/app/src/main/java/edu/ncsu/mobile/traces/MapsActivity.java
+++ android/Traces/app/src/main/java/edu/ncsu/mobile/traces/MapsActivity.java
@@ -7,10 +7,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -24,6 +21,7 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -67,6 +65,8 @@ public class MapsActivity extends FragmentActivity implements LocationListener,G
                 .build();
 
         setContentView(layout.activity_maps);
+        setUpMapIfNeeded();
+        mMap.setOnMapLongClickListener(this);
 
         search = new SearchView(MapsActivity.this);
         rel_layout = (RelativeLayout) findViewById(id.rl);
@@ -96,7 +96,6 @@ public class MapsActivity extends FragmentActivity implements LocationListener,G
 
             @Override
             public boolean onQueryTextSubmit(String query) {
-                // TODO Auto-generated method stub
 
                 if(query.trim().isEmpty()){
                     Toast.makeText(getBaseContext(), "Enter Location",
@@ -122,95 +121,34 @@ public class MapsActivity extends FragmentActivity implements LocationListener,G
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(id.map))
                     .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        centerMapToCurrentLocation();
-        //new Thread(new Runnable() {
-        ///    public void run() {
-                try {
-                    plotTweetsByDefaultLocation(myLocation);
-                } catch (Exception e) {
-                    Log.e(LOG_APPTAG, "Cannot retrieve geo locations", e);
-                    return;
-                }
-        //    }
-        //}).start();
-    }
-
     private void centerMapToCurrentLocation() {
-        //Zoom to current Location
         mMap.setMyLocationEnabled(true);
-        //LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        // Criteria criteria = new Criteria();
-        //String bestProvider = locationManager.getBestProvider(criteria, true);
         myLocation = LocationServices.FusedLocationApi.getLastLocation(googleAPI);
 
         if (myLocation != null) {
-            onLocationChanged(myLocation);
-            //locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
             double latitude = myLocation.getLatitude();
             double longitude = myLocation.getLongitude();
             LatLng latLng = new LatLng(latitude, longitude);
             mMap.addMarker(new MarkerOptions().position(latLng));
-            zoomToNewLocation(latLng);
+            plotTweetsByDefaultLocation();
         }
     }
 
-    private void plotTweetsByDefaultLocation(Location myLocation) {
-        //JSON Data for default location of user based on lat-lng[myLocation.getLatitude/Longitude()]
+    private void plotTweetsByDefaultLocation() {
         coordinateAPIQuery.lat = myLocation.getLatitude()+"";
         coordinateAPIQuery.lng = myLocation.getLongitude()+"";
         coordinateAPIQuery.rad = null;
@@ -359,8 +297,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener,G
 
     @Override
     public void onConnected(Bundle bundle) {
-        setUpMapIfNeeded();
-        mMap.setOnMapLongClickListener(this);
+        centerMapToCurrentLocation();
     }
 
     @Override
