0b7c34de8c6d10691dc7e00f0d3407decb118755
Vikas Piddempally <vpiddem@ncsu.edu>
2015-05-04 09:50:56 -0400

    PlayStore Version

--- android/Traces/app/src/main/java/edu/ncsu/mobile/traces/CustomAdapter.java
+++ android/Traces/app/src/main/java/edu/ncsu/mobile/traces/CustomAdapter.java
@@ -76,8 +76,9 @@ public class CustomAdapter extends BaseAdapter {
             final ImageView imageView_Profile = (ImageView) view.findViewById(R.id.listView_profileImage);
             final TextView textView_RtCount = (TextView) view.findViewById(R.id.textView_RtCount);
             final TextView textView_FavoriteCount = (TextView) view.findViewById(R.id.textView_FavoriteCount);
-            final WebView wv_TweetContent = (WebView) view.findViewById(R.id.webView_TweetContent);
-                viewHolder = new ViewHolder(textView_UserName, imageView_Profile, textView_TweetContent, textView_RtCount, textView_FavoriteCount, wv_TweetContent);
+//            final WebView wv_TweetContent = (WebView) view.findViewById(R.id.webView_TweetContent);
+                viewHolder = new ViewHolder(textView_UserName, imageView_Profile, textView_TweetContent, textView_RtCount, textView_FavoriteCount);
+//                viewHolder = new ViewHolder(textView_UserName, imageView_Profile, textView_TweetContent, textView_RtCount, textView_FavoriteCount, wv_TweetContent);
 
             view.setTag(viewHolder);
         } else {
@@ -107,30 +108,30 @@ public class CustomAdapter extends BaseAdapter {
         viewHolder.textView_RtCount.setText(arrayList.get(position).getRetweetCount() + "");
         viewHolder.textView_FavoriteCount.setText(arrayList.get(position).getFavoriteCount() + "");
 
-
-        if (!arrayList.get(position).getDataUrl().equalsIgnoreCase("")) {
-
-        /*Web View Load */
-            // Let's display the progress in the activity title bar, like the
-            // browser app does.
-            viewHolder.webView_TweetContent.getSettings().setJavaScriptEnabled(true);
-
-            viewHolder.webView_TweetContent.getSettings().setUseWideViewPort(true);
-            viewHolder.webView_TweetContent.getSettings().setLoadWithOverviewMode(true);
-            viewHolder.webView_TweetContent.getSettings().setDisplayZoomControls(true);
-
-            viewHolder.webView_TweetContent.setWebViewClient(new WebViewClient() {
-                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                    Toast.makeText(context, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-                }
-            });
-            viewHolder.webView_TweetContent.loadUrl(arrayList.get(position).getDataUrl());
-            viewHolder.webView_TweetContent.setVisibility(View.VISIBLE);
-        }
-        else{
-            viewHolder.webView_TweetContent.setVisibility(View.GONE);
-        }
-
+//
+//        if (!arrayList.get(position).getDataUrl().equalsIgnoreCase("")) {
+//
+//        /*Web View Load */
+//            // Let's display the progress in the activity title bar, like the
+//            // browser app does.
+//            viewHolder.webView_TweetContent.getSettings().setJavaScriptEnabled(true);
+//
+//            viewHolder.webView_TweetContent.getSettings().setUseWideViewPort(true);
+//            viewHolder.webView_TweetContent.getSettings().setLoadWithOverviewMode(true);
+//            viewHolder.webView_TweetContent.getSettings().setDisplayZoomControls(true);
+//
+//            viewHolder.webView_TweetContent.setWebViewClient(new WebViewClient() {
+//                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//                    Toast.makeText(context, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//            viewHolder.webView_TweetContent.loadUrl(arrayList.get(position).getDataUrl());
+//            viewHolder.webView_TweetContent.setVisibility(View.VISIBLE);
+//        }
+//        else{
+//            viewHolder.webView_TweetContent.setVisibility(View.GONE);
+//        }
+//
         //3. Return the inflated view here
         return view;
     }
@@ -146,15 +147,15 @@ public class CustomAdapter extends BaseAdapter {
 
         TextView textView_RtCount;
         TextView textView_FavoriteCount;
-        WebView webView_TweetContent;
+//        WebView webView_TweetContent;
 
-        public ViewHolder(TextView tv_UserName, ImageView imageView, TextView tv_TweetContent, TextView tv_RtCount, TextView tv_FavCount, WebView wv_TweetContent) {
+        public ViewHolder(TextView tv_UserName, ImageView imageView, TextView tv_TweetContent, TextView tv_RtCount, TextView tv_FavCount) {
             this.listView_UserTitle = tv_UserName;
             this.listView_profileImage = imageView;
             this.listView_tweet = tv_TweetContent;
             this.textView_RtCount = tv_RtCount;
             this.textView_FavoriteCount = tv_FavCount;
-            this.webView_TweetContent = wv_TweetContent;
+//            this.webView_TweetContent = wv_TweetContent;
         }
     }
 
